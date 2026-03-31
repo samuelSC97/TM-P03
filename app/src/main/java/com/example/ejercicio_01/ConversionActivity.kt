6e9f8d060ae0d607230930ab725c589129ab80ce
samuelSC97 <samuel.sucasaire97@gmail.com>
2026-03-31 00:01:29 -0500

    Ejercicio

---
+++ app/src/main/java/com/example/ejercicio_01/ConversionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ejercicio_01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConversionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conversion)
+    }
+}
